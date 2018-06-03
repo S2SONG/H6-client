03ad04057f8ac9a2bd3c7a8e753006246725e78b
ansongi <cherishu527@gmail.com>
2018-06-03 11:11:56 +0900

    no message

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //global 설정, 서버, 메인 배경 등..
 export default config = {
     // server: "http://13.125.225.253",
-    server: "http://192.168.219.102",
+    server: "http://192.168.200.150",
     main_background_color1: "#009DDF",
     main_background_color2: "#0D43A4",
     main_background_color3: "#0A2570"
